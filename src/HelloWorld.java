c9b99283e8dcab26357ca02c599ddc728100badf
FenixPJ <503098795@qq.com>
2021-03-16 10:11:25 +0800

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("===========");
+        System.out.println("HELLO WORLD");
+        System.out.println("===========");
+        System.out.println("2021-03-16 10:10 at DanDong");
+    }
+}
